4d04e964be89b21b770a6ac71730239aa442c98d
Chris He <chris7he@gmail.com>
2019-01-21 18:06:40 -0600

    Add Auth0 stuff; Will get to actually fixing this later

--- main.go
+++ main.go
@@ -1,14 +1,26 @@
 package main
 
 import (
+	"log"
+	"net/http"
+	"os"
 	"path"
 	"path/filepath"
 
-	"github.com/chelium/simple-website/handlers"
+	"github.com/auth0-community/go-auth0"
 	"github.com/gin-gonic/gin"
+	jose "gopkg.in/square/go-jose.v2"
+
+	"github.com/chelium/simple-website/handlers"
+)
+
+var (
+	audience string
+	domain   string
 )
 
 func main() {
+	setAuth0Variables()
 	r := gin.Default()
 	r.NoRoute(func(c *gin.Context) {
 		dir, file := path.Split(c.Request.RequestURI)
@@ -20,10 +32,40 @@ func main() {
 		}
 	})
 
+	authorized := r.Group("/")
+	authorized.Use(authRequired())
 	r.GET("/todo", handlers.GetTodoHandler)
 
 	err := r.Run(":3000")
 	if err != nil {
 		panic(err)
 	}
+}
+
+func setAuth0Variables() {
+	audience = os.Getenv("AUTH0_API_IDENTIFIER")
+	domain = os.Getenv("AUTH0_DOMAIN")
+}
+
+func authRequired() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var auth0Domain = "https://" + domain + "/"
+		client := auth0.NewJWKClient(auth0.JWKClientOptions{URI: auth0Domain + ".well-known/jwks.json"}, nil)
+		configuration := auth0.NewConfiguration(client, []string{audience}, auth0Domain, jose.RS256)
+		validator := auth0.NewValidator(configuration, nil)
+
+		_, err := validator.ValidateRequest(c.Request)
+
+		if err != nil {
+			log.Println(err)
+			terminateWithError(http.StatusUnauthorized, "token is not valid", c)
+			return
+		}
+		c.Next()
+	}
+}
+
+func terminateWithError(statusCode int, message string, c *gin.Context) {
+	c.JSON(statusCode, gin.H{"error": message})
+	c.Abort()
 }
